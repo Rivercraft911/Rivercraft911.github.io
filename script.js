4cfdafe3aca49457e06af13a2ed3a0b6bdb727de
Rivercraft911 <73358629+Rivercraft911@users.noreply.github.com>
2023-04-28 21:48:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+window.addEventListener('DOMContentLoaded', function() {
+    if (localStorage.getItem('buttonClicked')) {
+        disableButton();
+    }
+});
+
 document.getElementById('submitBtn').addEventListener('click', function(event) {
     event.preventDefault(); // Prevent the default behavior
     alert("They are cloning in the woods");
+    localStorage.setItem('buttonClicked', true);
+    disableButton();
+});
+
+function disableButton() {
     var button = document.getElementById('submitBtn');
     button.disabled = true;
     button.style.backgroundColor = '#cccccc';
     button.style.cursor = 'not-allowed';
-});
+}
