fd6633ddc0e5c6a5bb85a2171075a9ff397d0249
Rivercraft911 <73358629+Rivercraft911@users.noreply.github.com>
2023-04-28 21:40:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementById('submitBtn').addEventListener('click', function() {
     alert("They are cloning in the woods");
+    this.disabled = true;
+    this.style.backgroundColor = '#cccccc';
+    this.style.cursor = 'not-allowed';
 });
