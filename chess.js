5eb682eebdd0a2bdec6e6bcefd9aa783d1d21a92
Rivercraft911 <73358629+Rivercraft911@users.noreply.github.com>
2023-05-02 09:32:07 -0500

    Update chess.js

--- chess.js
+++ chess.js
@@ -1,188 +1,29 @@
-var board,
-    game = new Chess();
-
-/* board initialization and configuration starts here */
-
-var cfg = {
+document.addEventListener("DOMContentLoaded", function () {
+  const boardConfig = {
     draggable: true,
-    position: 'start',
-    onDragStart: onDragStart,
-    onDrop: onDrop,
-    onMouseoutSquare: onMouseoutSquare,
-    onMouseoverSquare: onMouseoverSquare,
-    onSnapEnd: onSnapEnd
-};
+    position: "start",
+    onDrop: onDrop
+  };
 
-board = ChessBoard('chessBoard', cfg);
+  const board = Chessboard("myBoard", boardConfig);
+  const game = new Chess();
 
-/* board initialization and configuration ends here */
-
-/* board visualization and games state handling starts here*/
-
-var calculateBestMove = function(game) {
-    var newGameMoves = game.ugly_moves();
-    var bestMove = null;
-    var bestValue = -9999;
-
-    newGameMoves.forEach(function(move) {
-        game.ugly_move(move);
-        var boardValue = -evalBoard(game.board());
-        game.undo();
-        if (boardValue > bestValue) {
-            bestValue = boardValue;
-            bestMove = move;
-        }
+  function onDrop(source, target) {
+    const move = game.move({
+      from: source,
+      to: target,
+      promotion: "q"
     });
 
-    return bestMove;
-};
-
-var evalBoard = function(board) {
-    var totalEvaluation = 0;
-
-    for (var row = 0; row < 8; row++) {
-        for (var col = 0; col < 8; col++) {
-            totalEvaluation += getPieceValue(board[row][col]);
-        }
-    }
-
-    return totalEvaluation;
-};
-
-var getPieceValue = function(piece) {
-    if (piece === null) {
-        return 0;
-    }
-
-    var pieceValue;
-
-    switch (piece.type) {
-        case 'p':
-            pieceValue = 10;
-            break;
-        case 'r':
-            pieceValue = 50;
-            break;
-        case 'n':
-            pieceValue = 30;
-            break;
-        case 'b':
-            pieceValue = 30;
-            break;
-        case 'q':
-            pieceValue = 90;
-            break;
-        case 'k':
-            pieceValue = 900;
-            break;
-        default:
-            pieceValue = 0;
-            break;
-    }
-
-    return piece.color === 'w' ? pieceValue : -pieceValue;
-};
-
-/* board visualization and games state handling ends here*/
-
-var onDragStart = function(source, piece, position, orientation) {
-    if (game.in_checkmate() === true || game.in_draw() === true ||
-        piece.search(/^b/) !== -1) {
-        return false;
-    }
-};
-
-var makeBestMove = function() {
-    var bestMove = getBestMove(game);
-    game.ugly_move(bestMove);
-    board.position(game.fen());
-    renderMoveHistory(game.history());
-    if (game.game_over()) {
-        alert('Game over');
-    }
-};
-
-var getBestMove = function(game) {
-    if (game.game_over()) {
-        alert('Game over');
-    }
-    var bestMove = null;
-    var stockfish = new Worker('https://cdnjs.cloudflare.com/ajax/libs/stockfish/13.0.1/stockfish.asm.js');
-
-    stockfish.onmessage = function(event) {
-        if (event.data.startsWith('bestmove')) {
-            bestMove = event.data.split(' ')[1];
-            game.move({ from: bestMove.slice(0, 2), to: bestMove.slice(2, 4) });
-            board.position(game.fen());
-            renderMoveHistory(game.history());
-        }
-    };
-
-    stockfish.postMessage('position fen ' + game.fen());
-    stockfish.postMessage('go depth 15');
-};
-
-var renderMoveHistory = function(moveHistory) {
-    var historyElement = $('#move-history').empty();
-    historyElement.empty();
-    for (var i = 0; i < moveHistory.length; i += 2) {
-        historyElement.append('<span>' + moveHistory[i] + ' ' +
-            ( moveHistory[i + 1] ? moveHistory[i + 1] : ' ') + '</span><br>');
-    }
-    historyElement.scrollTop(historyElement[0].scrollHeight);
-};
-
-var onDrop = function(source, target) {
-    var move = game.move({
-        from: source,
-        to: target,
-        promotion: 'q'
-    });
-
-    removeGreySquares();
     if (move === null) {
-        return 'snapback';
+      return "snapback";
     }
+  }
 
-    window.setTimeout(makeBestMove, 250);
-};
-
-var onMouseoverSquare = function(square, piece) {
-    var moves = game.moves({
-        square: square,
-        verbose: true
-    });
-
-    if (moves.length === 0) return;
-
-    greySquare(square);
-
-    for (var i = 0; i < moves.length; i++) {
-        greySquare(moves[i].to);
-    }
-};
-
-var onMouseoutSquare = function(square, piece) {
-    removeGreySquares();
-};
-
-var onSnapEnd = function() {
+  function newGame() {
+    game.reset();
     board.position(game.fen());
-};
+  }
 
-var removeGreySquares = function() {
-    $('#chessBoard .square-55d63').css('background', '');
-};
-
-var greySquare = function(square) {
-    var squareEl = $('#chessBoard .square-' + square);
-
-    var background = '#a9a9a9';
-    if (squareEl.hasClass('black-3c85d') === true) {
-        background = '#696969';
-    }
-
-    squareEl.css('background', background);
-};
-
-/* board helpers end here */
+  document.getElementById("newGameBtn").addEventListener("click", newGame);
+});
