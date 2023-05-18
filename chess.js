a8607447671bd8dfc35118f47c4b84241c3d19e7
Rivercraft911 <73358629+Rivercraft911@users.noreply.github.com>
2023-05-18 08:37:11 -0500

    Update chess.js

--- chess.js
+++ chess.js
@@ -1,4 +1,4 @@
-document.getElementById('giveUpBtn').addEventListener('click', function(event) {
+document.getElementById('workplease').addEventListener('click', function(event) {
     console.log('Button clicked'); // This will log 'Button clicked' to the console when the button is clicked
     event.preventDefault(); // Prevent the default behavior
     window.location.href = 'video.html'; // Redirect to the video page
