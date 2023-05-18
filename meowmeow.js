dbfca8ee5dc1da11db215f5c62b0d5046df98b77
Rivercraft911 <73358629+Rivercraft911@users.noreply.github.com>
2023-05-18 08:57:16 -0500

    Update meowmeow.js

--- meowmeow.js
+++ meowmeow.js
@@ -1,9 +1,9 @@
-document.getElementById('aiGenerated').addEventListener('click', function(event) {
+document.getElementById('nay').addEventListener('click', function(event) {
     event.preventDefault(); // Prevent the default behavior
     window.location.href = '.io'; // Redirect to the next page
 });
 
-document.getElementById('human').addEventListener('click', function(event) {
+document.getElementById('yay').addEventListener('click', function(event) {
     event.preventDefault(); // Prevent the default behavior
     alert('congrats!!'); // Show a message to the user to try again
 });
