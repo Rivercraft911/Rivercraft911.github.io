f5bb71cffa6c2093b6dd262f3eb5a4a9cf6ed6da
Rivercraft911 <73358629+Rivercraft911@users.noreply.github.com>
2023-05-18 08:20:47 -0500

    Create aiOrNot.js

---
+++ aiOrNot.js
@@ -0,0 +1,9 @@
+document.getElementById('leftGenerated').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default behavior
+    window.location.href = 'nextPage.html'; // Redirect to the next page if left is the correct answer
+});
+
+document.getElementById('rightGenerated').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default behavior
+    alert('Please try again.'); // Show a message to the user to try again if right is not the correct answer
+});
