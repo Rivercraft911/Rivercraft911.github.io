a37439658ef75b1ee3637a02dc33c7957dbaa3ec
Rivercraft911 <73358629+Rivercraft911@users.noreply.github.com>
2023-05-18 08:15:39 -0500

    Create chess.js

---
+++ chess.js
@@ -0,0 +1,4 @@
+document.getElementById('giveUpBtn').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default behavior
+    window.location.href = 'video.html'; // Redirect to the video page
+});
