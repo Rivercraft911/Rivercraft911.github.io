76132789f60098b34fd8e8e5c3a1070874180987
Rivercraft911 <73358629+Rivercraft911@users.noreply.github.com>
2023-05-18 08:42:28 -0500

    Update chess.js

--- chess.js
+++ chess.js
@@ -2,4 +2,4 @@ document.getElementById('workplease').addEventListener('click', function(event) 
     console.log('Button clicked'); // This will log 'Button clicked' to the console when the button is clicked
     event.preventDefault(); // Prevent the default behavior
     window.location.href = 'video.html'; // Redirect to the video page
-});
+}); 
