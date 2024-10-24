98cd5cb6f8e66400548f8d0f4ab127d2677efa62
Rivercraft911 <73358629+Rivercraft911@users.noreply.github.com>
2024-10-24 14:32:12 -0700

    Update meowmeow.js

--- meowmeow.js
+++ meowmeow.js
@@ -1,10 +1,10 @@
 document.getElementById('nay').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default behavior
-    window.location.href = 'index.html'; // Redirect to the original page
+    event.preventDefault();
+    window.location.href = 'index.html'; 
 });
 
 document.getElementById('yay').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default behavior
-    alert('Congrats!!'); // Show a message to the user
-    window.location.href = 'congrats.html'; // Redirect to the congratulations page
+    event.preventDefault();
+    alert('Congrats!!');
+    window.location.href = 'congrats.html';
 });
